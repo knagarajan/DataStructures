8cb43cdfbf944f735a9f53eb7aa27f57838d9ae1
knagarajan <nagarajan.karthik@gmail.com>
2011-08-03 01:36:02 -0500

    adding initial solution and projects in root folder

---
+++ DataStructures.Tests/NodeTest.cs
@@ -0,0 +1,23 @@
+﻿using DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DataStructures.Tests
+{
+    [TestClass()]
+    public class NodeTest
+    {
+        private Node _subject;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _subject = new Node();
+        }
+
+        [TestMethod()]
+        public void DefaultConstruction()
+        {
+            Assert.AreEqual(0, _subject.Data);
+        }
+    }
+}
